986c44103048a3b36d3df69dbf40ec7474e5bf7e
Hydshsh's Laptop <padpads.code@gmail.com>
2025-01-12 12:40:51 +0700

    Update mau bikin pertanyaan

---
+++ lib/pages/question.dart
@@ -0,0 +1,94 @@
+import 'package:app_kuis/models/quiz_question.dart';
+
+const question = [
+  QuizQuestion(
+    'Apa nama dunia tempat permainan Genshin Impact berlangsung?',
+    [
+      'Tevyat' //
+      'Mondstadt'
+      'Liyue'
+      'Inazuma'
+    ],
+  ),
+  QuizQuestion(
+    'Siapa nama karakter utama (protagonis) yang dapat kamu pilih di awal permainan?',
+    [
+      'Amber'
+      'Lumine atau Aether' //
+      'Diluc'
+      'Zhongli'
+    ],
+  ),
+  QuizQuestion(
+    'Elemen apa yang digunakan oleh karakter Amber di Genshin Impact?',
+    [
+      'Air (Hydro)'
+      'Api (Pyro)' //
+      'Angin (Anemo)'
+      'Batu (Geo)'
+    ],
+  ),
+  QuizQuestion(
+    'Apa tujuan utama Traveler (karakter utama) dalam cerita Genshin Impact?',
+    [
+      'Mencari saudara kembarnya yang hilang' //
+      'Menjadi petualang terhebat di Teyvat'
+      'Menyelamatkan dunia dari kehancuran'
+      'Mengalahkan Archon Electro'
+    ],
+  ),
+  QuizQuestion(
+    'Apa nama sistem gacha yang digunakan untuk mendapatkan karakter dan senjata baru di Genshin Impact?',
+    [
+      'Loot Box'
+      'Wish' //
+      'Summon'
+      'Spin'
+    ],
+  ),
+  QuizQuestion(
+    'Apa elemen yang digunakan oleh Barbara, karakter yang juga seorang idol di Mondstadt?',
+    [
+      'Api (Pyro)'
+      'Angin (Anemo)'
+      'Air (Hydro)' //
+      'Listrik (Electro)'
+    ],
+  ),
+  QuizQuestion(
+    'Apa nama mata uang premium yang digunakan untuk membeli Intertwined Fate dan Acquaint Fate di Genshin Impact?',
+    [
+      'Mora'
+      'Primogems' //
+      'Sigils'
+      'Genesis Crystals'
+    ],
+  ),
+  QuizQuestion(
+    'Berapa jumlah elemen yang ada di Genshin Impact?',
+    [
+      '5'
+      '6'
+      '7' //
+      '8'
+    ],
+  ),
+  QuizQuestion(
+    'Siapa Archon Angin (Anemo) yang memimpin Mondstadt?',
+    [
+      'Venti' //
+      'Jean'
+      'Diluc'
+      'Ningguang'
+    ],
+  ),
+  QuizQuestion(
+    'Apa nama kapal terbang yang digunakan oleh karakter di Genshin Impact untuk melayang di udara?',
+    [
+      'Glider' //
+      'Airship'
+      'Paraglider'
+      'Windcatcher'
+    ],
+  ),
+];
